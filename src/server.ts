66b409628bfb6f57260a867f6efa1a4e5ebf05b3
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-29 17:39:51 -0300

    :sparkles: feat(server): add swagger to the app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
-import router from './routes'
+import {router} from './routes'
 import "dotenv/config"
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
+
 const appPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(router)
 
 app.get("/terms", (req, res) => {
