0fe944be91234c0896ea48a9fc2a981203f06481
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-29 23:23:23 -0300

    :memo: doc(swagger+redoc): add redoc and improved swagger

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,23 @@ import express from 'express'
 import { router } from './routes'
 import "dotenv/config"
 import swaggerUi from 'swagger-ui-express'
-import swaggerDocs from './swagger.json'
+import swaggerDocs from '../swagger.json'
 
 const appPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.get("/swagger", (req, res) => {
+    return res.sendFile(process.cwd() + "/swagger.json")
+})
+app.get("/docs", (req, res) => {
+    return res.sendFile(process.cwd() + "/index.html")
+
+})
 app.get("/terms", (req, res) => {
     return res.json({ message: "Terms of service" })
 })
+
 app.use(router)
 app.listen(appPort, () => { console.log(`Server's running. PORT:${appPort}`) })
